2eef756e283c46f414e5a04305b8a46d23c620eb
Latifa AlSaidi <l.alsaidi@gamechangers.sa>
2022-08-17 15:35:13 +0300

    Test 2 (Circle color switcher based on timer with parenting)

---
+++ Assets/Scripts/SpriteSwitcherTest2.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSwitcherTest2 : MonoBehaviour
+{
+    SpriteTimer spriteTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteTimer = FindObjectOfType<SpriteTimer>();
+
+    }
+    void UpdateSprite()
+    {
+        if (spriteTimer.isWhite)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateSprite();
+    }
+}
