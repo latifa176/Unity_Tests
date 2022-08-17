37e68fdf8c623b3645ec7820cc6866a07d2f79ea
Latifa AlSaidi <l.alsaidi@gamechangers.sa>
2022-08-17 22:10:20 +0300

    Input if statements for Animated Capsule

--- Assets/Scripts/AnimatedCapsuleTest4.cs
+++ Assets/Scripts/AnimatedCapsuleTest4.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class AnimatedCapsuleTest4 : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+
+        }
     }
 }
